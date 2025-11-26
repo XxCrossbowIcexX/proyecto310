3c23a51f64c1c557e0884e6aa36be7ab20faf2ac
rodrigovivesuy <rodrigovivesuy@gmail.com>
2025-11-25 21:58:00 -0300

    Implementacion de endpoint POST /login en el backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
-// Importar Routers
 import catsRoutes from "./routes/categoriasRoutes.js";
 import productsRoutes from "./routes/productosRoutes.js";
 import commentsRoutes from "./routes/comentariosRoutes.js";
@@ -9,13 +9,43 @@ import ventasRoutes from "./routes/ventasRoutes.js";
 
 const app = express();
 const port = 3000;
+const JWT_SECRET = 'Clave_Secreta_Compartida_eMercado'; 
 
 app.use(cors());
 app.use(express.json());
 
+// Generación del Token
+app.post('/login', (req, res) => {
+    const { usuario, contraseña } = req.body; 
+
+    // Validación y Verificación de Credenciales
+    if (usuario === 'admin' && contraseña === 'jap2025') {
+        const user = { id: 1, name: usuario };
+        const token = jwt.sign(
+            user, 
+            JWT_SECRET, 
+            { expiresIn: '1h' } 
+        );
+
+        // Devuelve el token
+        return res.json({ 
+            mensaje: 'Autenticación exitosa', 
+            token: token 
+        });
+
+    } else {
+        // Login Fallido
+        return res.status(401).json({ 
+            mensaje: 'Usuario o contraseña incorrectos. Acceso denegado.' 
+        });
+    }
+});
+// -----------------------------
+
+
 // Ruta raíz
 app.get("/", (req, res) => {
-  res.send("¡Backend de eMercado JAP 2025 funcionando correctamente!");
+    res.send("¡Backend de eMercado JAP 2025 funcionando correctamente!");
 });
 
 app.use("/categorias", catsRoutes);
@@ -23,4 +53,4 @@ app.use("/productos", productsRoutes);
 app.use("/comentarios", commentsRoutes);
 app.use("/ventas", ventasRoutes);
 
-app.listen(port, () => console.log("Servidor iniciado correctamente"));
+app.listen(port, () => console.log(`Servidor iniciado correctamente en http://localhost:${port}`));
